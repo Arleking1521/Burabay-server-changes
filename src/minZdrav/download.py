c72b13c4cf3818e24bba250dc15f3800fa39fb3c
Arleking <Abdukarim600@gmail.com>
2024-04-10 00:53:16 +0500

    fix  download

--- src/minZdrav/download.py
+++ src/minZdrav/download.py
@@ -3,9 +3,9 @@ from django.conf import settings
 import os
 
 def download_file(request, file_name):
-    file_path = os.path.join(settings.MEDIA_ROOT, file_name)
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as f:
+    
+    if os.path.exists(file_name):
+        with open(file_name, 'rb') as f:
             response = HttpResponse(f.read(), content_type='application/octet-stream')
             response['Content-Disposition'] = f'attachment; filename="{file_name}"'
             return response
